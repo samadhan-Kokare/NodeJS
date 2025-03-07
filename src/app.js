026b2df9c2bca62e72e272cdb525bf0884f6b66c
Samadhan kokare <samadhankokare1999@gmail.com>
2025-03-07 16:06:43 +0530

    create get, post API

--- src/app.js
+++ src/app.js
@@ -1,8 +1,59 @@
 const express= require('express');
-const connectDB=require('./utils/database');
-const app  = express();
+const connectDB=require('./utils/database'); 
+const app  = express(); //creating an express app
+const User= require('./models/user'); //importing the user model
 
-const User= require('./models/user')
+app.use(express.json()); //middleware to parse the incoming request with json payload.
+
+app.post('/signup',async (req,res)=>{
+    //creating a new instance of the user model
+    const userObj=new User(req.body); 
+    console.log(req.body) 
+    try{
+        await userObj.save(); 
+        res.send('User added successfully');
+    }
+    catch(err){
+        res.status(400).send('something went wrong',err);
+        
+    }   
+});
+
+app.get('/user',async(req,res)=>{
+    try{
+        const users= await User.find({email:req.body.email});
+        if(users.length===0){
+            return res.status(404).send('User not found');
+        }
+        res.send(users);
+    }
+    catch(err){
+        res.status(400).send('something went wrong');
+    }
+});
+
+//find first user with given email
+app.get('/user1',async(req,res)=>{
+    try{
+        const users=await User.findOne({email:req.body.email});
+        if(!users){
+            return res.status(404).send('User not found');
+        }
+        res.send(users);
+    }catch(err){
+        res.status(400).send('Something went wrong');
+    }
+})
+
+app.get('/feed',async(req,res)=>{
+    try{
+        const users= await User.find({});
+        res.send(users);
+    }
+    catch(err){
+        res.status(400).send('something went wrong');
+    }
+})
 
 connectDB()
 .then(()=>{
@@ -14,25 +65,5 @@ connectDB()
 .catch((err)=>{
     console.log('Erro in connecting database',err);
 });
-
-app.post('/signup',async (req,res)=>{
-    //creating a new instance of the user model
-    const userObj=new User({
-        firstName:"Sai",
-        lastName:"Pallavi",
-        email:"saipallavi@18",
-        password:"SaiPallavi@🤞🫰"
-    });  
-    try{
-        await userObj.save(); 
-        res.send('User added successfully');
-    }
-    catch(err){
-        res.status(400).send('something went wrong',err);
-        
-    }
-    
-    
-})
  
 
